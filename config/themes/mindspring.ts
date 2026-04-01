c4896ad1d491acb77d91478af76f97cc4956b36c
Rachit <dekhlo30@gmail.com>
2026-04-01 20:47:45 +0530

    final touches

--- config/themes/mindspring.ts
+++ config/themes/mindspring.ts
@@ -236,7 +236,7 @@ export const mindspringTheme = {
   help: {
     hero: {
       backgroundImage:
-        'https://c4.wallpaperflare.com/wallpaper/710/467/622/digital-art-cgi-3d-question-mark-minimalism-hd-wallpaper-preview.jpg',
+        'https://plus.unsplash.com/premium_photo-1683865775849-b958669dca26?q=80&w=2064&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       alt: 'Support and guidance at Mindspring',
       tagline: 'Help Center',
       title: 'Answers for children and caregivers',
